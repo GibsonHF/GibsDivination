0e2a145d2e86bfc4af13a3d09924e8f2c268faa7
Conor <conormurray78@gmail.com>
2023-11-13 13:34:18 -0500

    include xapi in jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ tasks.named<Jar>("jar") {
 dependencies {
     implementation ("net.botwithus.rs3:botwithus-api:1.0.0-20231112.012705-1")
     implementation ("com.github.BotWithUs:BwuExtendedPublicAPI:master-SNAPSHOT")
+    "includeInJar" ("com.github.BotWithUs:BwuExtendedPublicAPI:master-SNAPSHOT")
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
